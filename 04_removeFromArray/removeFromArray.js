d11d154f85e20206ee57c92cc21fc3f74a004350
fishonttree <116611743+fishonttree@users.noreply.github.com>
2023-09-26 23:06:57 -0400

    Add solution to JS Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
 const removeFromArray = function() {
 
+    startArray = arguments[0] ;
+
+    for (i = 1 ; i < arguments.length ; i++) {
+        if (startArray.indexOf(arguments[i]) > -1) {
+            startArray.splice(startArray.indexOf(arguments[i]), 1) ;
+        }
+    }
+
+    console.log(startArray) ;
+
+    return startArray ;
+
 };
 
 // Do not edit below this line

8a5931cf69caa8769569e900eb9d793de80fbf39
fishonttree <116611743+fishonttree@users.noreply.github.com>
2023-09-26 22:27:59 -0400

    Add solution to JS Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatString, num) {
+    concatString = '' ;
+    if (num < 0) {
+        return 'ERROR' ;
+    }
+    for (i = 0 ; i < num ; i++) {
+        concatString += repeatString ;
+    }
+    return concatString ; 
 };
 
 // Do not edit below this line

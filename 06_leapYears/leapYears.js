742575830e7f15b10e760b28b7a7cd1505833c79
fishonttree <116611743+fishonttree@users.noreply.github.com>
2023-09-27 00:21:09 -0400

    Add solution to JS Exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    /* Divisible by 4, divisible by 100 if and only if divisible by 400 simultaneously */
+
+    if (year % 100 === 0 && year % 400 !== 0) {
+        return false ;
+    } else if (year % 100 === 0 && year % 400 === 0) {
+        return true ;
+    } else if (year % 4 === 0) {
+        return true ;
+    } else {
+        return false ;
+    }
 };
 
 // Do not edit below this line

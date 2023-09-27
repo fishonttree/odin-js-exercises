4c82e8a49424602a3f2d5e66b58d3bf05382c7ec
fishonttree <116611743+fishonttree@users.noreply.github.com>
2023-09-26 23:38:03 -0400

    Add solution to JS Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+
+    sum = 0 ;
+
+    if (startInt < 0 || endInt < 0 ) {
+        return "ERROR" ;
+    }
+
+    if (typeof startInt !== "number" || typeof endInt != "number") {
+        return "ERROR" ;
+
+        /* Otherwise, use Number.isInteger() */
+    }
+
+    if (startInt > endInt) {
+        temp = endInt ;
+        endInt = startInt ;
+        startInt = temp ;
+
+        /* Otherwise, just do [min, max] = [max, min] */
+    }
+
+    for (i = startInt ; i <= endInt ; i++) {
+        sum += i ; 
+    }
+
+    return sum ; 
 
 };
 

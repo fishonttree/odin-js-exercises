54242df1dbc0bee3cfd23de0e8497a29bc10944c
fishonttree <116611743+fishonttree@users.noreply.github.com>
2023-09-27 01:02:43 -0400

    Add solution to JS Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fDegree) {
 
-const convertToFahrenheit = function() {
+  cDegree = (fDegree - 32) * 5/9 ;
+
+  cDegree = Number.parseFloat(cDegree).toFixed(1) ;
+
+  console.log(typeof cDegree) ;
+
+  return Number(cDegree) ;
+
+} ;
+
+const convertToFahrenheit = function(cDegree) {
+
+  fDegree = (cDegree * 9/5 + 32) ;
+
+  fDegree = Number.parseFloat(fDegree).toFixed(1) ;
+
+  return Number(fDegree) ;
+
 };
 
 // Do not edit below this line
